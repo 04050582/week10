7b37b4b4614fa7c37bcbf87386e83017d776be4a
04050582 <hc920052@gmail.com>
2017-05-01 17:21:26 +0800

    Add files via upload

---
+++ lab01.ino
@@ -0,0 +1,179 @@
+const byte SP_Pin = 11;
+
+#define Do 261
+#define re 294
+#define mi 329
+#define fa 349
+#define so 392
+#define la 440
+#define si 493
+#define do2 523
+#define re2 587
+#define mi2 659
+#define fa2 698
+#define so2 784
+#define la2 880
+#define si2 988
+void setup()
+{
+  pinMode(SP_Pin,OUTPUT);
+}
+void loop()
+{
+  tone(SP_Pin,mi,500);
+  delay(300);
+  tone(SP_Pin,so,500);
+  delay(300);
+  tone(SP_Pin,so,500);
+  delay(400);
+  tone(SP_Pin,so,500);
+  delay(250);
+  tone(SP_Pin,mi,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,si,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(800);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,so,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(300);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,so,500);
+  delay(600);
+  
+  tone(SP_Pin,mi,500);
+  delay(250);
+  tone(SP_Pin,so,500);
+  delay(250);
+  tone(SP_Pin,so,300);
+  delay(300);
+  tone(SP_Pin,so,500);
+  delay(250);
+  tone(SP_Pin,mi,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,si,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(300);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,so,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,re2,500);
+  delay(600);
+
+  
+  tone(SP_Pin,so2,500);
+  delay(250);
+  tone(SP_Pin,so2,500);
+  delay(250);
+  tone(SP_Pin,so2,500);
+  delay(250);
+  tone(SP_Pin,so2,500);
+  delay(250);
+  tone(SP_Pin,mi2,500);
+  delay(250);
+  tone(SP_Pin,re2,500);
+  delay(500);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,re2,500);
+  delay(250);
+  tone(SP_Pin,re2,500);
+  delay(250);
+  tone(SP_Pin,re2,500);
+  delay(500);
+  tone(SP_Pin,re2,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,mi2,500);
+  delay(250);
+  tone(SP_Pin,re2,500);
+  delay(250);
+  
+  tone(SP_Pin,mi2,500);
+  delay(250);
+  tone(SP_Pin,mi2,500);
+  delay(250);
+  tone(SP_Pin,mi2,500);
+  delay(250);
+  tone(SP_Pin,mi2,500);
+  delay(250);
+  tone(SP_Pin,re2,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(500);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,re2,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(250);
+  tone(SP_Pin,so,500);
+  delay(250);
+  tone(SP_Pin,so,500);
+  delay(250);
+  tone(SP_Pin,so,500);
+  delay(250);
+  tone(SP_Pin,la,500);
+  delay(500);
+  tone(SP_Pin,so,500);
+  delay(250);
+  tone(SP_Pin,re2,500);
+  delay(250);
+  tone(SP_Pin,mi2,500);
+  delay(250);
+  tone(SP_Pin,do2,500);
+  delay(400);
+}
---
+++ lab02.ino
@@ -0,0 +1,91 @@
+#include <Keypad.h>
+#define NOTE_C3 261
+#define NOTE_CS3 139
+#define NOTE_D3 294
+#define NOTE_DS3 156
+#define NOTE_E3 329
+#define NOTE_F3 349
+#define NOTE_FS3 185
+#define NOTE_G3 392
+#define NOTE_GS3 329
+#define NOTE_A3 440
+#define NOTE_AS3 392
+#define NOTE_B3 493
+#define NOTE_C4 523
+#define NOTE_CS4 523
+#define NOTE_D4 587
+#define NOTE_DS4 644
+#define NOTE_E4 659
+
+#define NOTE_F4 698
+#define NOTE_FS4 880
+#define NOTE_G4 784
+#define NOTE_GS4 1046
+#define NOTE_A4 880
+#define NOTE_AS4 1175
+#define NOTE_B4 988
+#define WHOLE 1 //全分符
+#define HALF 0.5 //半分符
+#define QUARTER 0.25
+#define EIGHTH 0.125
+#define SIXTEENTH 0.0625
+int tune[] = { NOTE_B4, NOTE_A4, NOTE_G4,
+NOTE_F4, NOTE_E4, NOTE_D4, NOTE_C4,
+NOTE_B3, NOTE_A3, NOTE_G3, NOTE_F3,
+NOTE_E3, NOTE_D3, NOTE_C3,NOTE_GS4, NOTE_AS4};
+float duration[] = {QUARTER,
+QUARTER, QUARTER, QUARTER,
+QUARTER, QUARTER, QUARTER, QUARTER,
+QUARTER,QUARTER, QUARTER,
+QUARTER, QUARTER, QUARTER, QUARTER, QUARTER};
+
+
+int length = sizeof(tune);
+void setup() 
+{
+Serial.begin(9600); 
+   pinMode(11, OUTPUT);
+   int length = sizeof(tune); 
+   sizeof(tune[0]);
+
+}
+const byte ROWS = 4;
+const byte COLS = 4;
+char key;
+int k;
+char keys[ROWS][COLS]={
+{'F', 'E', 'D', 'C'}, 
+  {'B', '3', '6', '9'},
+  {'A', '2', '5', '8'}, 
+  {'0', '1', '4', '7'}
+  };
+
+byte rowPins[ROWS]={6,7,8,9};
+byte colPins[COLS]={2,3,4,5};
+
+Keypad keypad = Keypad(makeKeymap(keys),rowPins,colPins,ROWS,COLS);
+
+
+void loop() 
+{
+  
+  key = keypad.getKey();
+  
+  if (key != NO_KEY)
+  {
+    if(key>='0' && key<='9')
+    {
+       tone(11, tune[key-48]);
+       delay(100);
+       noTone(11);
+    }
+    if(key>='A' && key<='F')
+    {
+      tone(11, tune[key-65+10]);
+       delay(100);
+       noTone(11);
+     
+    }
+    Serial.println(key);
+ }
+}
